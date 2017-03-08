db2cd7b0629d5b829d5aaa1815b56bfae6d32f58
Ryan Jones (RIT Student) <rpj5582@ad.rit.edu>
2017-03-08 15:38:14 -0500

    Moved Cursor class to separate file

--- client/cursor.js
+++ client/cursor.js
@@ -1,1 +1,41 @@
+class Cursor {
+  constructor(socket, canvas, ctx, cursorImg) {
+    this.socket = socket;
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.cursorImg = cursorImg;
+    this.posX = 0;
+    this.posY = 0;
+    this.prevX = 0;
+    this.prevY = 0;
+    this.isDrawing = false;
 
+    // ---- Sets up canvas mouse input events
+    this.canvas.addEventListener('mousemove', (e) => {
+      const rect = this.canvas.getBoundingClientRect();
+
+      this.prevX = this.posX;
+      this.prevY = this.posY;
+
+      this.posX = e.clientX - rect.left;
+      this.posY = e.clientY - rect.top;
+    });
+
+    this.canvas.addEventListener('mousedown', (e) => {
+      this.isDrawing = true;
+    });
+
+    this.canvas.addEventListener('mouseup', (e) => {
+      this.isDrawing = false;
+    });
+
+    this.canvas.addEventListener('mouseout', () => {
+      this.isDrawing = false;
+    });
+    //----
+  }
+
+  draw() {
+    this.ctx.drawImage(this.cursorImg, this.posX, this.posY - this.cursorImg.height);
+  }
+}
