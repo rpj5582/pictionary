d8c894fd736874def02627503a8e28fe28091b83
Ryan Jones <rpj5582@rit.edu>
2017-03-08 01:28:50 -0500

    Finished work on drawing and chat functionality.

---
+++ client/app.js
@@ -0,0 +1,225 @@
+class Cursor {
+  constructor(socket, canvas, ctx, cursorImg) {
+    this.socket = socket;
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.cursorImg = cursorImg;
+    this.posX = 0;
+    this.posY = 0;
+    this.prevX = 0;
+    this.prevY = 0;
+    this.isDrawing = false;
+
+    // ---- Sets up canvas mouse input events
+    this.canvas.addEventListener('mousemove', (e) => {
+      const rect = this.canvas.getBoundingClientRect();
+
+      this.prevX = this.posX;
+      this.prevY = this.posY;
+
+      this.posX = e.clientX - rect.left;
+      this.posY = e.clientY - rect.top;
+    });
+
+    this.canvas.addEventListener('mousedown', (e) => {
+      this.isDrawing = true;
+    });
+
+    this.canvas.addEventListener('mouseup', (e) => {
+      this.isDrawing = false;
+    });
+
+    this.canvas.addEventListener('mouseout', () => {
+      this.isDrawing = false;
+    });
+    //----
+  }
+
+  draw() {
+    this.ctx.drawImage(this.cursorImg, this.posX, this.posY - this.cursorImg.height);
+  }
+}
+
+class App {
+  constructor() {
+    this.canvas = document.querySelector('canvas');
+    this.ctx = this.canvas.getContext('2d');
+
+    this.canvas.addEventListener('mousemove', (e) => {
+      if (this.cursor.isDrawing) {
+        const line = {
+          startX: this.cursor.prevX,
+          startY: this.cursor.prevY,
+          endX: this.cursor.posX,
+          endY: this.cursor.posY,
+          lineWidth: this.dynCtx.lineWidth,
+          strokeStyle: this.dynCtx.strokeStyle,
+        };
+
+        this.drawToDynamicCanvas(line);
+        this.sendDrawLineToServer(line);
+      }
+    });
+
+    this.dynCanvas = document.createElement('canvas');
+    this.dynCanvas.width = this.canvas.width;
+    this.dynCanvas.height = this.canvas.height;
+    this.dynCanvas.style = this.canvas.style;
+    this.dynCtx = this.dynCanvas.getContext('2d');
+
+    this.dynCtx.fillStyle = 'white';
+    this.dynCtx.fillRect(0, 0, this.dynCanvas.width, this.dynCanvas.height);
+    this.dynCtx.fillStyle = 'black';
+
+    const pencilRadioButton = document.querySelector('#pencilRadio');
+    const eraserRadioButton = document.querySelector('#eraserRadio');
+    const strokeSizeSlider = document.querySelector('#strokeSizeSlider');
+    const clearButton = document.querySelector('#clearButton');
+
+    this.chatHistory = document.querySelector('#chatHistory');
+    const chatBox = document.querySelector('#chatBox');
+    const sendChatButton = document.querySelector('#sendChatButton');
+
+    pencilRadioButton.addEventListener('click', () => {
+      this.dynCtx.strokeStyle = 'black';
+    });
+
+    eraserRadioButton.addEventListener('click', () => {
+      this.dynCtx.strokeStyle = 'white';
+    });
+
+    strokeSizeSlider.addEventListener('change', (e) => {
+      this.dynCtx.lineWidth = e.target.value;
+    });
+
+    clearButton.addEventListener('click', (e) => {
+      this.clear();
+      this.sendClearToServer();
+    });
+
+    sendChatButton.addEventListener('click', () => {
+      const message = chatBox.value;
+      chatBox.value = '';
+
+      this.socket.emit('msg', message);
+    });
+
+    this.dynCtx.lineWidth = strokeSizeSlider.value;
+    this.dynCtx.lineCap = 'round';
+    this.dynCtx.lineJoin = 'round';
+
+    this.socket = io.connect();
+
+    const pencilImg = document.querySelector('#pencil');
+    this.cursor = new Cursor(this.socket, this.canvas, this.ctx, pencilImg, this.dynCanvas);
+
+    this.socket.on('connect', () => {
+      this.onReconnecting();
+      this.onReconnect();
+      this.onReconnectFailed();
+      this.onDisconnect();
+      this.onLineDraw();
+      this.onClear();
+      this.onDrawHistory();
+      this.onMsg();
+    });
+  }
+
+  onReconnecting() {
+    this.socket.on('reconnecting', (attempt) => {
+      if (attempt === 1) {
+        this.addChatMsg('Attempting to reconnect...');
+      }
+    });
+  }
+
+  onReconnect() {
+    this.socket.on('reconnect', () => {
+      this.addChatMsg('Successfully reconnected to server.');
+    });
+  }
+
+  onReconnectFailed() {
+    this.socket.on('reconnect_failed', () => {
+      this.addChatMsg('Could not reconnect to the server.');
+    });
+  }
+
+  onDisconnect() {
+    this.socket.on('disconnect', () => {
+      this.addChatMsg('Lost connection to server.');
+    });
+  }
+
+  onLineDraw() {
+    this.socket.on('drawLine', this.drawToDynamicCanvas.bind(this));
+  }
+
+  onClear() {
+    this.socket.on('clear', () => {
+      this.clear();
+    });
+  }
+
+  onDrawHistory() {
+    this.socket.on('DrawHistory', (lines) => {
+      for (let i = 0; i < lines.length; i++) {
+        this.drawToDynamicCanvas(lines[i]);
+      }
+    });
+  }
+
+  onMsg() {
+    this.socket.on('msg', (message) => {
+      this.addChatMsg(message);
+    });
+  }
+
+  addChatMsg(message) {
+    this.chatHistory.value += `${message}\n`;
+  }
+
+  update() {
+    requestAnimationFrame(this.update.bind(this));
+
+    this.draw();
+  }
+
+  sendDrawLineToServer(line) {
+    this.socket.emit('drawLine', line);
+  }
+
+  sendClearToServer() {
+    this.socket.emit('clear');
+  }
+
+  clear() {
+    this.dynCtx.save();
+    this.dynCtx.fillStyle = 'white';
+    this.dynCtx.fillRect(0, 0, this.dynCanvas.width, this.dynCanvas.height);
+    this.dynCtx.restore();
+  }
+
+  drawToDynamicCanvas(line) {
+    this.dynCtx.save();
+    this.dynCtx.lineWidth = line.lineWidth;
+    this.dynCtx.strokeStyle = line.strokeStyle;
+    this.dynCtx.beginPath();
+    this.dynCtx.moveTo(line.startX, line.startY);
+    this.dynCtx.lineTo(line.endX, line.endY);
+    this.dynCtx.stroke();
+    this.dynCtx.closePath();
+    this.dynCtx.restore();
+  }
+
+  draw() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.drawImage(this.dynCanvas, 0, 0);
+    this.cursor.draw();
+  }
+}
+
+window.onload = () => {
+  const app = new App();
+  app.update();
+};
--- src/server.js
+++ src/server.js
@@ -1,32 +1,145 @@
 const http = require('http');
 const socketio = require('socket.io');
+const fs = require('fs');
 
+// ---- Sets up the web server
 const PORT = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  response.end();
+  switch (request.url) {
+    case '/cursor.js': {
+      fs.readFile(`${__dirname}/../client/cursor.js`, (cursorError, cursorData) => {
+        if (cursorError) throw cursorError;
+
+        response.writeHead(200, { 'Content-Type': 'application/javascript' });
+        response.write(cursorData);
+        response.end();
+      });
+      break;
+    }
+
+    case '/app.js': {
+      fs.readFile(`${__dirname}/../client/app.js`, (appError, appData) => {
+        if (appError) throw appError;
+
+        response.writeHead(200, { 'Content-Type': 'application/javascript' });
+        response.write(appData);
+        response.end();
+      });
+      break;
+    }
+
+    case '/pencil.png': {
+      fs.readFile(`${__dirname}/../client/pencil.png`, (pngError, pngData) => {
+        if (pngError) throw pngError;
+
+        response.writeHead(200, { 'Content-Type': 'image/png' });
+        response.write(pngData);
+        response.end();
+      });
+      break;
+    }
+
+    case '/main.css': {
+      fs.readFile(`${__dirname}/../client/main.css`, (cssError, cssData) => {
+        if (cssError) throw cssError;
+
+        response.writeHead(200, { 'Content-Type': 'text/css' });
+        response.write(cssData);
+        response.end();
+      });
+      break;
+    }
+
+    default: {
+      fs.readFile(`${__dirname}/../client/index.html`, (htmlError, htmlData) => {
+        if (htmlError) throw htmlError;
+
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write(htmlData);
+        response.end();
+      });
+      break;
+    }
+  }
 };
 
 const app = http.createServer(onRequest).listen(PORT);
 
 console.log(`Listening on 127.0.0.1:${PORT}`);
+//----
 
+// Creates the socketio server
 const io = socketio(app);
 
-const onJoin = (sock) => {
+// A list of users
+this.users = {};
+this.userCount = 0;
+
+// A list of line objects, acting as a draw hsitory
+this.lines = [];
+
+const onDrawLine = (sock) => {
   const socket = sock;
+
+  socket.on('drawLine', (line) => {
+    this.lines.push(line);
+    socket.broadcast.to('room1').emit('drawLine', line);
+  });
 };
 
-const onLeave = (sock) => {
+const onClear = (sock) => {
   const socket = sock;
+
+  socket.on('clear', () => {
+    this.lines = [];
+    socket.broadcast.to('room1').emit('clear');
+  });
 };
 
-io.sockets.on('connection', (socket) => {
-  console.log('Connection opened');
+const onMsg = (sock) => {
+  const socket = sock;
 
-  onJoin(socket);
-  onLeave(socket);
+  socket.on('msg', (message) => {
+    io.sockets.in('room1').emit('msg', `${socket.user.name}: ${message}`);
+  });
+};
+
+const onDisconnect = (sock) => {
+  const socket = sock;
+
+  socket.on('disconnect', () => {
+    console.log(`${socket.user.name} has disconnected`);
+
+    io.sockets.in('room1').emit('msg', `${socket.user.name} has left the room.`);
+
+    delete this.users[socket.user.name];
+  });
+};
+
+// Called when a user connects to the server
+io.sockets.on('connection', (sock) => {
+  const socket = sock;
+
+  console.log('A new user has connected');
+
+  const username = `User ${++this.userCount}`;
+  this.users[username] = {
+    name: username,
+  };
+
+  socket.user = this.users[`User ${this.userCount}`];
+  socket.join('room1');
+
+  // Sets up all the events
+  onDrawLine(socket);
+  onClear(socket);
+  onMsg(socket);
+  onDisconnect(socket);
+
+  io.sockets.in('room1').emit('msg', `${socket.user.name} has join the room.`);
+
+  socket.emit('drawHistory', this.lines);
 });
 
 console.log('Websocket server started');
